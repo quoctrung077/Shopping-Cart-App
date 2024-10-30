f4d1dc72d09d4d0f268ac942e806056c6b003c1b
Kay Huynh <quoctrung077@gmail.com>
2024-10-30 16:06:13 +0700

    layout view-product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Products from "../components/Products";
 function Home() {
   return (
     <>
       <Header />
+      <Products />
       <Footer />
     </>
   );
