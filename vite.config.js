ab20f0a36d625714b426e1e87064c70e05ba473d
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2024-11-04 14:23:52 +0700

    fix config vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Lắng nghe trên tất cả các địa chỉ IP
-    port: 3000, // Hoặc port bạn muốn dùng
-  },
-})
+	plugins: [react()],
+	base: "/", // Thay đổi nếu cần thiết
+	server: {
+		host: "0.0.0.0", // Lắng nghe trên tất cả các địa chỉ IP
+		port: 3000, // Hoặc port bạn muốn dùng
+		historyApiFallback: true, // Hỗ trợ cho React Router
+	},
+});
